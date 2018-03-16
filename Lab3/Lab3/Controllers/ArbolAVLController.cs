89d9878f032a714f61e4667e159df14378fdfcb1
Dennis <dennisgarcialg2@gmail.com>
2018-03-15 23:22:06 -0600

    Lab3HR

---
+++ Lab3/Lab3/Controllers/ArbolAVLController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Lab3.Controllers
+{
+    public class ArbolAVLController : Controller
+    {
+        // GET: ArbolAVL
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // This action handles the form POST and the upload
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the filename
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+            // redirect back to the index action to show the form once again
+            return RedirectToAction("Create");
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
