943c45a115116f712fac0e92704ed3cfc08b8a3c
Dennis <dennisgarcialg2@gmail.com>
2018-04-19 01:46:13 -0600

    Revert "Revert "Patadas de Ahogado""
    This reverts commit cfa1e6b445bb5b27ae1d05699b3bf06a280f545f.

---
+++ EDDProyectoPreliminarHD/EDDProyectoPreliminarHD/Controllers/CuentaUsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EDDProyectoPreliminarHD.Models;
+
+namespace EDDProyectoPreliminarHD.Controllers
+{
+    public class CuentaUsuarioController : Controller
+    {
+        // GET: CuentaUsuario
+        public ActionResult Index()
+        {
+            //List<CuentaUsuario> listaUsuarios = new List<CuentaUsuario>();
+
+            return View();
+        }
+    }
+}
---
+++ MVCWALOROEDD/MVCWALOROEDD/Models/MyDbcontext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCWALOROEDD.Models
+{
+    public class MyDbcontext : DbContext
+    {
+        //Esta es una colección de todos los usuarios en el contexto
+        public DbSet<CuentaUsuario> cuentaUsuario { get; set; }
+    }
+}
