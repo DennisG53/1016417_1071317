7e07f5ee3c65028be0813a8a910da1173927c2b8
Dennis <dennisgarcialg2@gmail.com>
2018-04-18 23:56:57 -0600

    Login and Register UI

--- Proyecto_MVC/Proyecto_MVC/App_Start/RouteConfig.cs
+++ Proyecto_MVC/Proyecto_MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Proyecto_MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CuentaUsuario", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
--- Proyecto_MVC/Proyecto_MVC/Controllers/CuentaUsuarioController.cs
+++ Proyecto_MVC/Proyecto_MVC/Controllers/CuentaUsuarioController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Proyecto_MVC.Models;
 
 namespace Proyecto_MVC.Controllers
 {
@@ -13,5 +14,38 @@ namespace Proyecto_MVC.Controllers
         {
             return View();
         }
+
+        public ActionResult RegistrarUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarUser(CuentaUsuario cuenta)
+        {
+            if (ModelState.IsValid)
+            {
+                //Agregar a la lista
+
+                //Waiting
+
+                ModelState.Clear();
+                ViewBag.Message = cuenta.Username + " registrado correctamente.";
+            }
+            return View();
+        }
+
+        //Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Login(CuentaUsuario user)
+        //{
+
+        //}
+
     }
 }
